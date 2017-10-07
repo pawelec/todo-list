611e685817362f7e15fccc4e28ef79967253331a
Paweł Pawelec <kontakt@ppawelec.pl>
2017-10-07 13:10:44 +0200

    Add footer component to app.component.html and some styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 // components
 import { AppComponent } from './app.component';
+import { FooterComponent } from "./shared/footer/footer.component";
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 // other
 
@@ -12,6 +13,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
     PageNotFoundComponent,
   ],
   imports: [
