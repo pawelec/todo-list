b6427d6ecad1c1b7c1e972fcfcdb63e08952d339
Paweł Pawelec <kontakt@ppawelec.pl>
2017-10-07 11:57:13 +0200

    Add specs for '404 component'

---
+++ src/app/page-not-found/page-not-found.component.spec.ts
@@ -0,0 +1,41 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
+
+import { PageNotFoundComponent } from './page-not-found.component';
+
+describe('PageNotFoundComponent', () => {
+
+  let componentInstance:    PageNotFoundComponent;
+  let fixture: ComponentFixture<PageNotFoundComponent>;
+  let de:      DebugElement;
+  let header, paragraph:      HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PageNotFoundComponent ],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PageNotFoundComponent);
+
+    componentInstance = fixture.componentInstance;
+
+    de = fixture.debugElement.query(By.css('h1'));
+    header = de.nativeElement;
+    de = fixture.debugElement.query(By.css('p'));
+    paragraph = de.nativeElement;
+  });
+
+  it("should display title", () => {
+    fixture.detectChanges();
+    expect(header.textContent).toContain(componentInstance.title);
+  });
+
+  it('should display explanation', () => {
+    fixture.detectChanges();
+    expect(paragraph.textContent).toContain(componentInstance.explanation);
+  });
+});
