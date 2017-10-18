bde894e8952211f2c0f5f54dda53c3df302f4afc
Paweł Pawelec <kontakt@ppawelec.pl>
2017-10-18 19:10:36 +0200

    Add Angular CommonModule

--- src/app/todos/todos.module.ts
+++ src/app/todos/todos.module.ts
@@ -1,19 +1,23 @@
 // angular
 import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from "@angular/forms";
 // components
 import { TodosComponent } from "./todos.component";
 import { TodosInputComponent } from "./input/todos-input.component";
+import { TodosListComponent } from "./list/todos-list.component";
 // services
 import { TodosService } from "./todos.service";
 
 @NgModule({
   imports: [
+    CommonModule,
     ReactiveFormsModule
   ],
   declarations: [
     TodosComponent,
-    TodosInputComponent
+    TodosInputComponent,
+    TodosListComponent
   ],
   providers: [
       TodosService
