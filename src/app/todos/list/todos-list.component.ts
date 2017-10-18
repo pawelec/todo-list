c66ebd1df79c8565b22a4453a927ff6b4a87cfd9
Paweł Pawelec <kontakt@ppawelec.pl>
2017-10-18 18:52:14 +0200

    Add component for list

---
+++ src/app/todos/list/todos-list.component.ts
@@ -0,0 +1,11 @@
+// angular
+import { Component, OnInit, Input } from "@angular/core";
+
+@Component({
+    selector: 'todos-list',
+    templateUrl: './todos-list.component.html'
+})
+
+export class TodosListComponent {
+    
+}
