4bfc7464f97f3746d5c06f1fe45e82757ea71297
Paweł Pawelec <kontakt@ppawelec.pl>
2017-11-15 20:21:36 +0100

    Add more tests for IsNull extension method

--- Tests/TodoCommon.Tests/ObjectsExtensionsTests.cs
+++ Tests/TodoCommon.Tests/ObjectsExtensionsTests.cs
@@ -10,11 +10,41 @@ namespace TodoCommon.Tests
         [Theory]
         [InlineData(null, true)]
         [InlineData(default(string), true)]
+        [InlineData(default(Object), true)]
+        [InlineData("", false)]
+        [InlineData("Not null string", false)]
         internal void IsNull_ReferencesObjects_ShouldReturnExceptedValue(
           object obj, bool excepted)
         {
           bool isNull = obj.IsNull();
           isNull.ShouldBe(excepted);
         }
+
+      [Theory]
+      [InlineData(default(int), false)]
+      [InlineData(1, false)]
+      [InlineData(true, false)]
+      internal void IsNull_ValueTypeObjects_ShouldReturnExceptedValue(
+            object obj, bool excepted)
+      {
+        bool isNull = obj.IsNull();
+        isNull.ShouldBe(excepted);
+      }
+
+      [Fact]
+      internal void IsNull_NullableIntWithNull_ShouldReturnTrue()
+      {
+      Nullable<int> obj = null;
+        bool isNull = obj.IsNull();
+        isNull.ShouldBe(true);
+      }
+
+    [Fact]
+    internal void IsNull_NullableIntWithValue_ShouldReturnTrue()
+    {
+      Nullable<int> obj = 5;
+      bool isNull = obj.IsNull();
+      isNull.ShouldBe(false);
     }
+  }
 }
