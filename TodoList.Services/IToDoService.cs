28acd6df9ccce2f2838f8572c8d67616702f69d0
Paweł Pawelec <kontakt@ppawelec.pl>
2017-11-15 21:10:24 +0100

    Move TodoService to other file

--- TodoList.Services/IToDoService.cs
+++ TodoList.Services/IToDoService.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace TodoList.Services
@@ -9,29 +7,4 @@ namespace TodoList.Services
     TodoItem Get(int id);
     TodoItem Add(string name);
     }
-
-  public class TodoService : IToDoService
-  {
-    private IEnumerable<TodoItem> todos = new List<TodoItem>
-    {
-      new TodoItem { Id = 1, Name = "Test name" }
-    };
-    
-    public TodoItem Add(string name)
-    {
-      if (name == null)
-      {
-        throw new ArgumentNullException(nameof(name));
-      }
-
-      if (string.IsNullOrWhiteSpace(name))
-      {
-        throw new ArgumentException("Argument can not be blank", nameof(name));
-      }
-
-      throw new NotImplementedException();
-    }
-
-    public TodoItem Get(int id) => this.todos.FirstOrDefault(todoItem => todoItem.Id == id);
-  }
 }
---
+++ TodoList.Services/TodoService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoList.Services
+{
+  public class TodoService : IToDoService
+  {
+    private IEnumerable<TodoItem> todos = new List<TodoItem>
+    {
+      new TodoItem { Id = 1, Name = "Test name" }
+    };
+    
+    public TodoItem Add(string name)
+    {
+      if (name == null)
+      {
+        throw new ArgumentNullException(nameof(name));
+      }
+
+      if (string.IsNullOrWhiteSpace(name))
+      {
+        throw new ArgumentException("Argument can not be blank", nameof(name));
+      }
+
+      throw new NotImplementedException();
+    }
+
+    public TodoItem Get(int id) => this.todos.FirstOrDefault(todoItem => todoItem.Id == id);
+  }
+}
