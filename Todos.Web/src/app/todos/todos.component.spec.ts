d4aa818d5a2bf976c2f1e156949ea58e334fdd6c
Paweł Pawelec <kontakt@ppawelec.pl>
2017-12-11 22:28:54 +0100

    Add HttpClient import statement to spec file

--- Todos.Web/src/app/todos/todos.component.spec.ts
+++ Todos.Web/src/app/todos/todos.component.spec.ts
@@ -4,14 +4,16 @@ import { TodosInputComponent } from './input/todos-input.component';
 import { TestBed, ComponentFixture, async } from "@angular/core/testing";
 import { FormBuilder, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { TodosService } from "./todos.service";
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('TodosComponent', function () {
     let component: TodosComponent;
     let fixture: ComponentFixture<TodosComponent>;
+    let todosService: TodosService;
 
     beforeEach(async() => {
         TestBed.configureTestingModule({
-            imports: [ReactiveFormsModule, FormsModule],
+            imports: [ReactiveFormsModule, FormsModule, HttpClientTestingModule],
             declarations: [ TodosComponent, TodosInputComponent, TodosListComponent ],
             providers: [ FormBuilder, TodosService ],
         })
