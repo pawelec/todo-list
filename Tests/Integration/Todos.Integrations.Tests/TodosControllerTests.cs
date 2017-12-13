a8e4aeb4beca0a0abf9e247450294abc70145483
Paweł Pawelec <kontakt@ppawelec.pl>
2017-12-13 09:45:26 +0100

    Add integration test for Create and MarkAsDone API methods

--- Tests/Integration/Todos.Integrations.Tests/TodosControllerTests.cs
+++ Tests/Integration/Todos.Integrations.Tests/TodosControllerTests.cs
@@ -65,5 +65,58 @@ namespace Todos.Integrations.Tests
             // Assert
             response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
         }
+
+        [Fact]
+        internal async Task Create_CorrectValue_ReturnTodoItem()
+        {
+            // Arrange
+            var todoForCreation = new TodoForCreationDto { Value = "Test" };
+            var getReponse = await Context.Client.GetAsync("/api/todos");
+            var todos = await getReponse.Content.ReadAsJsonAsync<IEnumerable<TodoItem>>() as IEnumerable<TodoItem>;
+            int actualCount = todos.Count();
+            int expectedCount = actualCount + 1;
+
+            // Act
+            var postReponse = await Context.Client.PostAsJsonAsync("/api/todos", todoForCreation);
+            var newTodo = await postReponse.Content.ReadAsJsonAsync<TodoItem>() as TodoItem;
+
+            getReponse = await Context.Client.GetAsync("/api/todos");
+            todos = await getReponse.Content.ReadAsJsonAsync<IEnumerable<TodoItem>>() as IEnumerable<TodoItem>;
+            actualCount = todos.Count();
+
+            // Assert
+            postReponse.EnsureSuccessStatusCode();
+            postReponse.StatusCode.ShouldBe(HttpStatusCode.Created);
+            newTodo.Name.ShouldBe(todoForCreation.Value);
+
+            actualCount.ShouldBe(expectedCount);
+        }
+
+        [Fact]
+        internal async Task MarkAsDone_ItemNotExist_NotFoundStatusCode()
+        {
+            // Act
+            var putResponse = await Context.Client.PutAsync(
+                "/api/todos/-1", new StringContent("-1"));
+
+            // Assert
+            putResponse.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        internal async Task MarkAsDone_ItemExist_OkStatusCode()
+        {
+            // Arrange
+            var todoForCreation = new TodoForCreationDto { Value = "Test" };
+            var postReponse = await Context.Client.PostAsJsonAsync("/api/todos", todoForCreation);
+            var newTodo = await postReponse.Content.ReadAsJsonAsync<TodoItem>() as TodoItem;
+
+            // Act
+            var putResponse = await Context.Client.PutAsync(
+                $"/api/todos/{newTodo.Id}", new StringContent(newTodo.Id.ToString()));
+
+            // Assert
+            putResponse.StatusCode.ShouldBe(HttpStatusCode.OK);
+        }
     }
 }
